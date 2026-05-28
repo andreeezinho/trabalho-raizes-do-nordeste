f25e71441ea3881406db0faba41718ea1139eea3
andreeezinho <andreeesapucaia@gmail.com>
2026-05-28 19:03:08 -0300

    feat: create google auth service

---
+++ src/Infra/Services/Google/GoogleAuthService.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Infra\Services\Google;
+
+use Google\Client;
+use Google\Service\Oauth2;
+use GuzzleHttp\Client as GuzzleClient;
+use Google\Service\Oauth2\Userinfo;
+
+class GoogleAuthService {
+
+    private Client $client;
+    private Userinfo $data;
+
+    public function __construct() {
+        $this->client = new Client();
+    }
+
+    public function init() {
+        $guzzleClient = new GuzzleClient(['curl' => [CURLOPT_SSL_VERIFYPEER => false]]);
+        $this->client->setHttpClient($guzzleClient);
+        $this->client->setAuthConfig(json_decode($_ENV['GOOGLE_CREDENTIALS'], true));
+        $this->client->setRedirectUri($_ENV['GOOGLE_REDIRECT_URI']);
+        $this->client->addScope('email');
+        $this->client->addScope('profile');
+    }
+
+    public function authorized($code) {
+        if(isset($code)){
+            $token = $this->client->fetchAccessTokenWithAuthCode($code);
+            $this->client->setAccessToken($token['access_token']);
+            $googleService = new Oauth2($this->client);
+
+            $this->data = $googleService->userinfo->get();
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public function getClientData() : Userinfo {
+        return $this->data;
+    }
+
+    public function generateAuthLink() {
+        return $this->client->createAuthUrl();
+    }
+
+}
