62d1ab294c12ff2c25b73da20803db99d41c99dc
andreeezinho <andreeesapucaia@gmail.com>
2026-05-30 21:46:37 -0300

    feat: create order_product controller file

---
+++ src/Http/Controllers/Pedido/PedidoProdutoController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Pedido;
+
+use App\Http\Request\Request;
+use App\Http\Controllers\Controller;
+use App\Domain\Repositories\Pedido\PedidoProdutoRepositoryInterface;
+use App\Domain\Repositories\Pedido\PedidoRepositoryInterface;
+use App\Domain\Repositories\Produto\ProdutoRepositoryInterface;
+use App\Http\Transformer\Pedido\PedidoProdutoTransformer;
+
+class PedidoProdutoController extends Controller {
+
+    
+
+}
