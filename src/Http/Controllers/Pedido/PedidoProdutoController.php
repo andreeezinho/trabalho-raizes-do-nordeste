43308b8f0227fc0ad77888741bea67c6089eda4b
andreeezinho <andreeesapucaia@gmail.com>
2026-05-31 09:35:05 -0300

    feat: create order_product controller methods

--- src/Http/Controllers/Pedido/PedidoProdutoController.php
+++ src/Http/Controllers/Pedido/PedidoProdutoController.php
@@ -11,6 +11,118 @@ use App\Http\Transformer\Pedido\PedidoProdutoTransformer;
 
 class PedidoProdutoController extends Controller {
 
-    
+    protected $produtoPedidoRepository;
+    protected $pedidoRepository;
+    protected $produtoRepository;
+    protected $pedidoProdutoTransformer;
+
+    public function __construct(
+        PedidoProdutoRepositoryInterface $produtoPedidoRepository,
+        PedidoRepositoryInterface $pedidoRepository,
+        ProdutoRepositoryInterface $produtoRepository,
+        PedidoProdutoTransformer $pedidoProdutoTransformer
+    ){
+        $this->produtoPedidoRepository = $produtoPedidoRepository;
+        $this->pedidoRepository = $pedidoRepository;
+        $this->produtoRepository = $produtoRepository;
+        $this->pedidoProdutoTransformer = $pedidoProdutoTransformer;
+    }
+
+    public function index(Request $request){
+        $params = $request->all();
+
+        $pedidoProduto = $this->produtoPedidoRepository->all($params);
+
+        return $this->respJson([
+            'message' => 'Pedidos e produtos listados',
+            'data' => $this->pedidoProdutoTransformer->transformArray($pedidoProduto)
+        ]);
+    }
+
+    public function store(Request $request){
+        $data = $request->all();
+
+        $validate = $this->validate($data, [
+            'quantidade' => 'required|int',
+        ]);
+
+        if(is_null($validate)){
+            return $this->respJson([
+                'message' => 'Dados inválidos',
+                'errors' => $this->getErrors()
+            ], 422);
+        }
+
+        $pedidoProduto = $this->produtoPedidoRepository->create($data);
+
+        if(is_null($pedidoProduto)){
+            return $this->respJson([
+                'message' => 'Não foi possível cadastrar produto no pedido'
+            ], 500);
+        }
+
+        return $this->respJson([
+            'message' => 'Cadastro realizado com sucesso',
+            'data' => $this->pedidoProdutoTransformer->transform($pedidoProduto)
+        ], 201);
+    }
+
+    public function update(Request $request, $uuid){
+        $data = $request->all();
+
+        $pedidoProduto = $this->produtoPedidoRepository->findBy('uuid', $uuid);
+
+        if(is_null($pedidoProduto)){
+            return $this->respJson([
+                'message' => 'Produto no pedido não encontrado'
+            ], 422);
+        }
+
+        $validate = $this->validate($data, [
+            'quantidade' => 'required|int',
+        ]);
+
+        if(is_null($validate)){
+            return $this->respJson([
+                'message' => 'Dados inválidos',
+                'errors' => $this->getErrors()
+            ], 422);
+        }
+
+        $pedidoProduto = $this->produtoPedidoRepository->update($data, $pedidoProduto->id);
+
+        if(is_null($pedidoProduto)){
+            return $this->respJson([
+                'message' => 'Não foi possível atualizar produto no pedido'
+            ], 500);
+        }
+
+        return $this->respJson([
+            'message' => 'Sucesso ao atualizar produto no pedido',
+            'data' => $this->pedidoProdutoTransformer->transform($pedidoProduto)
+        ], 201);
+    }
+
+    public function destroy(Request $request, $uuid){
+        $pedidoProduto = $this->produtoPedidoRepository->findBy('uuid', $uuid);
+
+        if(is_null($pedidoProduto)){
+            return $this->respJson([
+                'message' => 'Produto no pedido não encontrado'
+            ], 422);
+        }
+
+        $pedidoProduto = $this->produtoPedidoRepository->delete($pedidoProduto->id);
+
+        if(!$pedidoProduto){
+            return $this->respJson([
+                'message' => 'Não foi possível deletar produto no pedido'
+            ], 500);
+        }
+
+        return $this->respJson([
+            'message' => 'Produto no pedido deletado'
+        ], 201);
+    }
 
 }
